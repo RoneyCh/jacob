ae309cd4092e666e1ca51f6ced6665eda17e8a3a
roneycm <roneychristian159@gmail.com>
2023-10-26 13:33:05 -0300

    feat: loading inicial

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/*isLoading ? (
+        {isLoading ? (
           <Stack.Screen
             name="Loading"
             component={LoadingScreen}
@@ -37,7 +37,7 @@ const App: React.FC = () => {
               headerShown: false,
             }}
           />
-        ) :*/ (
+        ) : (
           <>
             <Stack.Screen
               name="Home"
